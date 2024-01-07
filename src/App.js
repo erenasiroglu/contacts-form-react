e074ea5af4149a0bd025c09e90d79717571989d0
Eren Nasıroğlu <erenasiroglu1@gmail.com>
2024-01-07 23:46:12 +0300

    improved code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import { useState, useEffect } from 'react';
-import Form from './components/Form'
+import Form from './components/Form';
 import List from './components/List';
+import './App.css';
 
 function Contacts() {
-  const [contacts, setContacts] = useState([])
+  const [contacts, setContacts] = useState([
+    {
+      fullName: 'Eren',
+      phoneNumber: '123456',
+    },
+    {
+      fullName: 'GitHub',
+      phoneNumber: '123456',
+    },
+  ]);
 
   useEffect(() => {
-    console.log(contacts)
-  }, [contacts])
+    console.log(contacts);
+  }, [contacts]);
+
   return (
-    <div>
-      <List/>
-      <Form addContact={setContacts} contacts={contacts}/>
+    <div className="contacts-container">
+      <div className="list-container">
+        <List contacts={contacts} />
+      </div>
+      <div className="form-container">
+        <Form addContact={setContacts} contacts={contacts} />
+      </div>
     </div>
   );
 }
--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import { useState } from 'react'
+import './styles.css'
 
-function List() {
+function List({ contacts}) {
+  const [filterText, setFilterText] = useState('')
+
+  const filtered = contacts.filter((item) => {
+    return Object.keys(item).some((key) => 
+      item[key]
+      .toString()
+      .toLowerCase()
+      .includes(filterText.toLocaleLowerCase())
+    )
+  })
+
   return (
     <div>
-      List
+      <input className="input-container" placeholder='Filter Contact' value={filterText} 
+      onChange={(e) => 
+        setFilterText(e.target.value)} />
+      <ul>
+        { filtered.map((contact, i) => (
+        <li key={i}>{contact.fullName}</li>
+        ))}
+      </ul>
     </div>
   )
 }
